6c5a64aff5f122b696013b47cd5c4c7608e1d32f
Lázaro Josué <jcslazaro@yahoo.com.br>
2021-09-15 17:34:21 -0300

    Correção do Pom.xml com Build, Anotations na classe produto

--- src/main/java/app/Principal.java
+++ src/main/java/app/Principal.java
@@ -1,10 +1,25 @@
 package app;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Produto;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Alfaloja-PU");
+		EntityManager em = emf.createEntityManager();
+		
+		//buscar um produto do DB
+		Produto objpro = em.find(Produto.class, 2);
+		System.out.println(objpro.getNome());
+		
+		em.close();
+		emf.close();
+		
 	}
 
 }
